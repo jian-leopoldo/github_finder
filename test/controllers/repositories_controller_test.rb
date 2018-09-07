5c650aba199bcefb0cb223bbc0765faf6fce2725
Jian Rodrigues <jian_rodrigues@hotmail.com>
2018-09-06 22:54:27 -0300

    basic controller test added

--- test/controllers/repositories_controller_test.rb
+++ test/controllers/repositories_controller_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class RepositoriesControllerTest < ActionDispatch::IntegrationTest
 
+  test "should get success message" do
+    get repositories_url
+    assert_response :success
+  end
+
   test "should get repositories from github" do
     get repositories_url
     assert Repository.all.count > 1
